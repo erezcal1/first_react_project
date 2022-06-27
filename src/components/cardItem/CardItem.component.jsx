e99cc3681cdaeb4ea470fcd545a6eb870a0e96e6
Erez Calderon <erezcal1@gmail.com>
2022-06-27 10:18:32 +0300

    amazon fake

--- src/components/cardItem/CardItem.component.jsx
+++ src/components/cardItem/CardItem.component.jsx
@@ -1,16 +1,17 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
-// import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
-
 const CardItemComponent = ({ img, title, id, onDeleteItem }) => {
   const handle_Button_Click = () => {
     onDeleteItem(id);
   };
   return (
     <div className="col">
-      <div className="card card-width">
-        <h5 className="card-title">{title}</h5>
-        <img src={img} className="card-img-top" alt={title}></img>
+      <div className="card border-0">
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <img src={img} className="card-img-top" alt={title}></img>
+          <a href="#" className="">
+            Go somewhere
+          </a>
+        </div>
       </div>
     </div>
   );
