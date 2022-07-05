bf03e3056ad66314bd7af6e451ee1393529566bc
Erez Calderon <erezcal1@gmail.com>
2022-07-05 11:03:22 +0300

    deleteCards

--- src/pages/userCards/userCards.page.jsx
+++ src/pages/userCards/userCards.page.jsx
@@ -2,79 +2,103 @@ import { Fragment, useEffect, useState } from "react";
 import axios from "axios";
 import UserCardsComponent from "../../components/userCards/userCards.component";
 import { cloneDeep } from "lodash";
+import { toast } from "react-toastify";
 
 const UserCardsPage = () => {
   const [userCards, setUserCards] = useState([]);
+
+  useEffect(() => {
+    // handleBtnClick();
+    getAllCards();
+  }, []);
   const onDeleteCard = (id) => {
+    //delete from server - axios.delete
+    //delete from state - setUserCards
+
     axios
       .delete(`/cards/${id}`)
       .then((res) => {
-        console.log(res);
         let newArray = cloneDeep(userCards);
         setUserCards(newArray.filter((card) => card._id !== id));
-        console.log(newArray);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+        toast.error("cannot delete the selected card", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      });
   };
-  useEffect(() => {
-    handleBtnClick();
-  }, []);
-  const handleBtnClick = () => {
+  const getAllCards = () => {
+    /*
+        getAllCards will send ajax get request to the server
+        and will get array of biz cards
+        then it will update the cardsArr state
+        if it will fail then it will create toast popup
+    */
     axios
-      .get("/cards")
+      .get(`/cards`)
       .then((res) => {
         console.log(res.data);
         setUserCards(res.data);
-        let newArr = [];
-        let inArr = [];
-        let l = res.data.length;
-        for (let i = 0; i < l; i++) {
-          if (i > 0 && i % 4 === 0) {
-            newArr = [
-              ...newArr,
-              <div className="row" key={i + "cards row"}>
-                {[...inArr]}
-              </div>,
-            ];
-            inArr = [];
-          }
-          inArr = [
-            ...inArr,
-            <div key={res.data[i]._id} className="col">
-              <UserCardsComponent
-                className="col"
-                card={res.data[i]}
-                onDeleteCard={onDeleteCard}
-              ></UserCardsComponent>
-            </div>,
-          ];
-        }
-        if (inArr.length > 0) {
-          newArr = [
-            ...newArr,
-            <div className="row" key={l + "cards row"}>
-              {[...inArr]}
-            </div>,
-          ];
-        }
-        setUserCards(newArr);
       })
-      .catch((e) => {
-        console.log(e);
+      .catch((error) => {
+        console.log("error", error);
+        toast.error("cannot get cards", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
       });
   };
-  return (
-    <Fragment>
-      <button className="btn btn-success" onClick={handleBtnClick}>
-        Send Request
-      </button>
-      {/* <div className="row">
-        {userCards.map((card, idx) => {
-          return <UserCardsComponent key={idx} {...card}></UserCardsComponent>;
-        })}
-      </div> */}
-      {userCards}
-    </Fragment>
-  );
+
+  const renderRowsFromArr = (arrOfCards) => {
+    /*
+        renderRowsFromArr will receive array of bizCards
+        and will create html elms to display the bizCards
+        in the page
+    */
+    let newArr = [];
+    let inArr = [];
+    let length = arrOfCards.length;
+    for (let i = 0; i < length; i++) {
+      if (i > 0 && i % 3 === 0) {
+        newArr = [
+          ...newArr,
+          <div className="row" key={i + "cards row"}>
+            {[...inArr]}
+          </div>,
+        ];
+        inArr = [];
+      }
+      inArr = [
+        ...inArr,
+        <div key={arrOfCards[i]._id} className="col">
+          <UserCardsComponent
+            card={arrOfCards[i]}
+            onDeleteCard={onDeleteCard}
+          ></UserCardsComponent>
+        </div>,
+      ];
+    }
+    if (inArr.length > 0) {
+      newArr = [
+        ...newArr,
+        <div className="row" key={length + "cards row"}>
+          {[...inArr]}
+        </div>,
+      ];
+    }
+  };
+  return <Fragment>{renderRowsFromArr(userCards)}</Fragment>;
 };
 export default UserCardsPage;
